68a16636042dd17945e22e51904cc7cbdbebbcd1
ThorOnTheRocks <gianluca.galota@gmail.com>
2023-01-04 07:57:26 +0100

    auth middleware

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,39 @@
+import jwt from 'jsonwebtoken';
+
+export const createJWT = (user) => {
+  const token = jwt.sign({
+    id: user.id, 
+    username: user.username
+  }, 
+  process.env.JWT_SECRET)
+  return token
+}
+
+export const protect = (req, res, next) => {
+  const bearer = req.headers.authorization;
+
+  if(!bearer) {
+    res.status(401)
+    res.json({message: 'not auhtorized!'})
+    return
+  }
+
+  const [, token] = bearer.split(' ');
+
+  if (!token) {
+    res.status(401)
+    res.json({message: 'not valid token!'})
+    return
+  }
+
+  try {
+    const user = jwt.verify(token, process.env.JWT_SECRET)
+    req.user = user
+    next()
+  } catch (error) {
+    console.error(error)
+    res.status(401)
+    res.json({message: 'not valid token!'})
+    return
+  }
+}
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import router from './router';
 import morgan from 'morgan';
 import cors from 'cors';
+import { protect } from './middlewares/auth';
 
 const app = express();
 
@@ -22,6 +23,6 @@ app.get('/', (req, res) => {
   res.json({ msg: 'hello' });
 })
 
-app.use('/api', router)
+app.use('/api', protect, router)
 
 export default app;
