11d9c8115e0281238729cdd535174ca9650fb5a8
ThorOnTheRocks <gianluca.galota@gmail.com>
2023-01-11 07:05:58 +0000

    validating inputs done

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import { body, oneOf } from 'express-validator';
+import { handleInputErrors } from './middlewares/inputErrors';
 
 const router = Router();
 
@@ -9,7 +11,12 @@ router.get('/product', (req, res) => {
   res.json({msg: 'message'})
 })
 router.get('/product/:id', () => {})
-router.put('/product/:id', () => {})
+router.put(
+  '/product/:id', 
+  body('name').isString(), 
+  handleInputErrors, 
+  (req, res) => { }
+)
 router.post('/product', () => {})
 router.delete('/product/:id', () => {})
 
@@ -18,17 +25,36 @@ router.delete('/product/:id', () => {})
 */
 router.get('/update', () => {})
 router.get('/update/:id', () => {})
-router.put('/update/:id', () => {})
-router.post('/update', () => {})
+router.put('/update/:id', 
+  body('title').optional(), 
+  body('body').optional(), 
+  body('status').isIn(['IN_PROGRESS', 'SHIPPED', 'DEPRECATED']), 
+  body('version').optional(), 
+  () => {}
+)
+router.post('/update', 
+  body('title').exists().isString(), 
+  body('body').exists().isString(), 
+  () => {}
+)
 router.delete('/update/:id', () => {})
 
 /**
  * Update Point
 */
-router.get('/updatepoint', () => {})
+router.get('/updatepoint',
+  body('name').optional().isString(), 
+  body('description').optional().isString(), 
+  () => {}
+)
 router.get('/updatepoint/:id', () => {})
 router.put('/updatepoint/:id', () => {})
-router.post('/updatepoint', () => {})
+router.post('/updatepoint', 
+  body('name').isString(), 
+  body('description').isString(),
+  body('updateId').exists().isString(),
+  () => {}
+)
 router.delete('/updatepoint/:id', () => {})
 
 export default router;
