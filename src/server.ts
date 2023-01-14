43e788aa38c1fe5ede4708e4efecb844b055c2a2
ThorOnTheRocks <gianluca.galota@gmail.com>
2023-01-14 19:47:13 +0000

    testing config done

--- src/server.ts
+++ src/server.ts
@@ -19,8 +19,6 @@ app.use(express.urlencoded({extended: true}));
 app.use(customLogger('custom logger'))
 
 app.get('/', (req, res) => {
-  console.log('Hello from express!');
-  res.status(200);
   res.json({ msg: 'hello' });
 })
 
